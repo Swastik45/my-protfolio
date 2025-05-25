af6418a4d5fd01c738c93929c0ddec19414cc49f
Swastik45 <149481053+Swastik45@users.noreply.github.com>
2025-05-25 17:53:28 +0545

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBOtAGp9JCDLHtgXh-YaYrbjVVgzmzvEyk",
-    authDomain: "protfolio-542a8.firebaseapp.com",
-    projectId: "protfolio-542a8",
-    storageBucket: "protfolio-542a8.appspot.com", 
-    messagingSenderId: "390517797984",
-    appId: "1:390517797984:web:3ca91ba89274d83284ff05",
-    measurementId: "G-25XHYD5K9W"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "", 
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
